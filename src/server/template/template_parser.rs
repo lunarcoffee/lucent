d4e83a3f9afa1f377ef8792d14aca4f28f9b4d22
LunarCoffee <lunarcoffee.pjc@gmail.com>
2021-07-19 23:35:05 -0400

    commenta

--- src/server/template/template_parser.rs
+++ src/server/template/template_parser.rs
@@ -1,5 +1,6 @@
 use crate::server::template::{Template, TemplatePart};
 
+// Parser for reading template files into a sequence of their parts.
 pub struct TemplateParser {
     file: String,
     parts: Vec<TemplatePart>,
@@ -18,38 +19,53 @@ impl TemplateParser {
         Some(Template { parts })
     }
 
+    // Attempts to parse the file into a sequence of template parts.
     fn parse_parts(mut self) -> Option<Vec<TemplatePart>> {
         let chars = self.file.chars().collect::<Vec<_>>();
         let mut pos = 0;
 
         while pos < chars.len() {
             self.parts.push(match chars[pos] {
+                // Beginning a single-value placeholder.
                 '[' => {
+                    // Find the end of the placeholder and extract its name.
                     let end_index = chars[pos..].iter().position(|c| *c == ']')? + pos;
                     let name = chars[pos + 1..end_index].iter().collect();
 
                     pos = end_index + 1;
                     TemplatePart::Placeholder(name)
                 }
+                // Beginning a multi-value placeholder.
                 '*' => {
+                    // Find the start of this placeholder.
                     let start_index = chars[pos..].iter().position(|c| *c == '[')? + pos;
+
+                    // Find the end.
                     let mut depth = 0;
                     let end_index = chars[start_index + 1..].iter().position(|c| {
+                        // Adjust the depth when encountering the start or end of a placeholder.
                         depth += "] [".find(|ch| ch == *c).unwrap_or(1) as i32 - 1;
+
+                        // If we've hit the end of a placeholder and the depth has gone negative, that means we've
+                        // exited the current placeholder, so this is the index of its end.
                         *c == ']' && depth < 0
                     })? + start_index + 1;
+
+                    // Extract the template for the values of this placeholder and try parsing it.
                     let sub_template = chars[start_index + 1..end_index].iter().collect();
+                    let parts = TemplateParser::new(sub_template).parse()?;
 
                     let name = chars[pos + 1..start_index].iter().collect();
-                    let parts = TemplateParser::new(sub_template).parse()?;
 
                     pos = end_index + 1;
                     TemplatePart::MultiplePlaceholder(name, parts)
                 }
+                // Skip the character following any '\'.
                 '\\' => {
                     pos += 2;
                     TemplatePart::String(chars[pos - 1].to_string())
                 }
+                // Nothing special, extract everything until the next special character.
                 _ => {
                     let start_of_next_part = chars[pos..]
                         .iter()
@@ -58,6 +74,7 @@ impl TemplateParser {
                         + pos;
                     let text = chars[pos..start_of_next_part].iter().collect();
 
+                    // Jump to the next special character.
                     pos = start_of_next_part;
                     TemplatePart::String(text)
                 }
